6c574b8efc67d26a0454c3f488a4678e9487c41a
Shawn Gabriel Intong <146860293+ShawnGabriel@users.noreply.github.com>
2025-01-10 22:36:49 -0800

    Update decoder.py

--- src/decoder.py
+++ src/decoder.py
@@ -1,12 +1,42 @@
 import torch
 from torch import nn
 from torch.nn import functional as F
-from attention import SelfAttention
+from sd.attention import SelfAttention
 
 class VAE_AttentionBlock(nn.Module):
     def __init__(self, channels: int):
         super().__init__()
+        # Normalization is done to avoid oscillation of the loss function as it makes the training slower
+        # Batch Norm. -> we normalize by cols (features)
+        # Layer Norm. -> we normalize by rows (data items)
         self.groupnorm = nn.GroupNorm(32, channels)
+        self.attention = SelfAttention(1, channels)
+        
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # (Batch_Size, Features, Height, Width)
+        
+        residue = x
+        
+        n, c, h, w = x.shape
+        
+        # (Batch_Size, Features, Height, Width) -> (Batch_Size, Features, Height * Width)
+        x = x.view(n, c, h * w)
+        
+        # (Batch_Size, Features, Height * Width) -> (Batch_Size, Height * Width, Features)
+        x = x.transpose(-1, -2)
+        
+        # (Batch_Size, Features, Height * Width) -> (Batch_Size, Height * Width, Features)
+        x = self.attention(x)
+        
+        # (Batch_Size, Height * Width, Features) -> (Batch_Size, Features, Height * Width)
+        x = x.transpose(-1, -2)
+        
+        # (Batch_Size, Features, Height * Width) -> (Batch_Size, Features, Height, Width)
+        x = x.view((n, c, h, w))
+        
+        x += residue
+        
+        return x
     
 
 class VAE_ResidualBlock(nn.Module):
@@ -42,4 +72,69 @@ class VAE_ResidualBlock(nn.Module):
         
         return x + self.residual_layer(residue)
     
+class VAE_Decoder(nn.Sequential):
     
+    def __init__(self):
+        super().__init__(
+            nn.Conv2d(3, 3, kernel_size=1, padding=0),
+            
+            nn.Conv2d(3, 512, kernel_size=3, padding=1),
+        
+            VAE_ResidualBlock(512, 512),
+            
+            VAE_AttentionBlock(512),
+            
+            VAE_ResidualBlock(512, 512),
+            
+            VAE_ResidualBlock(512, 512),
+            
+            VAE_ResidualBlock(512, 512),
+            
+            # (Batch_Size, 512, Height / 8, Width / 8) -> (Batch_Size, 512, Height / 8, Width / 8)
+            VAE_ResidualBlock(512, 512),
+            
+            # (Batch_Size, 512, Height / 8, Width / 8) -> (Batch_Size, 512, Height / 4, Width / 4)
+            nn.Upsample(scale_factor=2),
+            
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            
+            VAE_ResidualBlock(512, 512),
+            VAE_ResidualBlock(512, 512),
+            VAE_ResidualBlock(512, 512),
+            
+            # (Batch_Size, 512, Height / 4, Width / 4) -> (Batch_Size, 512, Height / 2, Width / 2)
+            nn.Upsample(scale_factor=2),
+            
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            
+            VAE_ResidualBlock(512, 256),
+            VAE_ResidualBlock(256, 256),
+            VAE_ResidualBlock(256, 256),
+            
+            # (Batch_Size, 512, Height / 2, Width / 2) -> (Batch_Size, 256, Height, Width)
+            nn.Upsample(scale_factor=2),
+            
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            
+            VAE_ResidualBlock(256, 128),
+            VAE_ResidualBlock(128, 128),
+            VAE_ResidualBlock(128, 128),
+            
+            nn.GroupNorm(32, 128),
+            
+            nn.SiLU(),
+            
+            # (Batch_Size, 128, Height, Width) -> (Batch_Size, 3, Height, Width)
+            nn.Conv2d(128, 3, kernel_size=3, padding=1)
+        )
+        
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # x: (Batch_Size, 4, Height / 8, Width / 8)
+        
+        x /= 0.1825
+        
+        for module in self:
+            x = module(x)
+        
+        # (Batch_Size, 3, Height, Width)
+        return x
